339e08ab68cb3fdda767febec14f6aa1c0a37d62
AveryDoesMagic <155497437+AveryDoesMagic@users.noreply.github.com>
2024-06-23 21:25:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-function toggleMode() {
-    var body = document.body;
-    var modeToggle = document.getElementById('mode-toggle');
-    var modeIcon = document.getElementById('mode-icon');
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const navLinks = document.querySelector('.nav-links');
 
-    body.classList.toggle('dark-mode');
-    
-    if (body.classList.contains('dark-mode')) {
-        // Switch to dark mode
-        modeIcon.innerHTML = '&#9790;'; // Unicode for moon icon
-    } else {
-        // Switch to light mode
-        modeIcon.innerHTML = '&#9728;'; // Unicode for sun icon
-    }
-}
+    hamburgerMenu.addEventListener('click', function () {
+        navLinks.classList.toggle('active');
+    });
+});
